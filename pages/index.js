5dba04a23901649262098fb6ccc6438786041da7
Zeph LaFassett! <zeph@lafassett.com>
2022-01-21 08:07:25 -0800

    [nextjs-setup] got linting to work

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from 'next/head'
 
 export default function Home () {
   return (
@@ -9,7 +9,7 @@ export default function Home () {
       </Head>
 
       <main>
-        <h1 className="title">
+        <h1 className='title'>
           Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
 
