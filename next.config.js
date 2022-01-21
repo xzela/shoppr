71d33c7e158a251203924f6dfae3c954520e6807
Zeph LaFassett! <zeph@lafassett.com>
2022-01-21 08:49:54 -0800

    [nextjs-setup] adding initial nextjs stuff

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const nextConfig = {
+	"compilerOptions": {
+		"baseUrl": "./src"
+	},
+	dir: './src'
+}
+
+module.exports = nextConfig
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Header from '../components/header'
+
+export default function Home () {
+  return (
+    <div className='container'>
+      <Header>
+        <title>Shoppr</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Header>
+
+      <main>
+		  <p>Word</p>
+		  </main>
+    </div>
+  )
+}
